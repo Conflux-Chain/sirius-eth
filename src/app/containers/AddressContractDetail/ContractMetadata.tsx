940ee6e3c8cc7ceff050c3750cea191c85a60a20
tangxuezhi <0x74616e67@gmail.com>
2022-02-25 17:05:40 +0800

    feat: remove contract admin in contract detail

--- src/app/containers/AddressContractDetail/ContractMetadata.tsx
+++ src/app/containers/AddressContractDetail/ContractMetadata.tsx
@@ -12,14 +12,12 @@ import { List } from 'app/components/List/';
 import { useTranslation } from 'react-i18next';
 import { translations } from 'locales/i18n';
 import { useToken } from 'utils/api';
-import { IconButton } from './IconButton';
 import { media } from 'styles/media';
 import { Text } from 'app/components/Text';
-import { Link as UILink } from '@cfxjs/react-ui';
 import { Tooltip } from 'app/components/Tooltip/Loadable';
 import SkeletonContainer from 'app/components/SkeletonContainer/Loadable';
 import { AddressContainer } from 'app/components/AddressContainer';
-import { isZeroAddress, formatString } from 'utils';
+import { formatString } from 'utils';
 import {
   ICON_DEFAULT_TOKEN,
   NETWORK_TYPES,
@@ -30,98 +28,6 @@ import {
 import { Image } from '@cfxjs/antd';
 
 const Link = ({ to, children }) => <RouterLink to={to}>{children}</RouterLink>;
-
-const WarnningButton = ({ address }) => {
-  const { t, i18n } = useTranslation();
-
-  return (
-    <WarnningButtonWrapper>
-      <IconButton
-        className="metadata-tooltip-btn"
-        size={16}
-        tooltipContentClassName="warnning-tooltip"
-        viewBox={!isZeroAddress(address) ? '0 0 1024 1024' : '0 0 16 16'}
-        tooltipText={
-          <WarnningTooltipWrapper>
-            <p className="warnning-text">
-              {t(translations.contractDetail.contractAdminWarning)}
-              <br />
-              <UILink
-                target="_blank"
-                className="warnning-tooltip-link"
-                href={
-                  i18n.language?.startsWith('zh')
-                    ? 'https://juejin.im/post/6876330619798814728'
-                    : 'https://github.com/Conflux-Chain/conflux-rust/tree/master/internal_contract#admincontrol-contract'
-                }
-              >
-                {t(translations.general.viewMore)}
-              </UILink>
-            </p>
-          </WarnningTooltipWrapper>
-        }
-      >
-        {!isZeroAddress(address) ? (
-          <path
-            d="M501.28 4.16a501.248 501.248 0 1 0 0 1002.56 501.248 501.248 0 0 0 0-1002.56z m42.24 668.8c0 23.36-19.2 42.24-42.24 42.24-23.04 0-42.24-19.136-42.24-42.24 0-23.04 19.2-42.24 42.24-42.24 23.04 0 42.24 19.2 42.24 42.24z m0-176.576a41.856 41.856 0 0 1-42.24 41.408 41.856 41.856 0 0 1-42.24-41.408V284.16c0-22.848 19.2-41.408 42.24-41.408 23.04 0 42.24 18.56 42.24 41.408v212.288z"
-            fill="#FFB84B"
-            p-id={950}
-          />
-        ) : (
-          <g
-            id="Internal-Contract"
-            stroke="none"
-            strokeWidth="1"
-            fill="none"
-            fillRule="evenodd"
-          >
-            <g
-              id="Icon-Up：16px-18px"
-              transform="translate(-992.000000, -319.000000)"
-            >
-              <g id="Success@2x" transform="translate(992.000000, 319.000000)">
-                <path
-                  d="M8,16 C12.4182667,16 16,12.4182667 16,8 C16,3.58172 12.4182667,0 8,0 C3.58172,0 0,3.58172 0,8 C0,12.4182667 3.58172,16 8,16 Z"
-                  id="safety"
-                  fill="#7CD77B"
-                ></path>
-                <polyline
-                  id="safety"
-                  stroke="#FFFFFF"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  points="4.66666667 7.88073333 7.21846667 10.4848667 11.1060667 5.33333333"
-                ></polyline>
-              </g>
-            </g>
-          </g>
-        )}
-      </IconButton>
-    </WarnningButtonWrapper>
-  );
-};
-
-const WarnningButtonWrapper = styled.div`
-  .warnning-tooltip.tooltip-content {
-    max-width: 30rem;
-    text-align: center;
-    padding: 0.43rem 0.86rem;
-    ${media.s} {
-      max-width: 15rem;
-    }
-  }
-  .icon.metadata-tooltip-btn {
-    margin-left: 1rem;
-  }
-`;
-const WarnningTooltipWrapper = styled.div`
-  p.warnning-text {
-    margin: 0;
-  }
-  .warnning-tooltip-link.link {
-    color: #008dff;
-  }
-`;
 
 export function ContractMetadata({ address, contractInfo }) {
   const { t } = useTranslation();
@@ -257,111 +163,6 @@ export function ContractMetadata({ address, contractInfo }) {
         </SkeletonContainer>
       ),
     },
-    {
-      title: (
-        <Tooltip
-          text={t(translations.toolTip.contract.contractAdmin)}
-          placement="top"
-        >
-          {t(translations.contract.contractAdmin)}
-        </Tooltip>
-      ),
-      children: (
-        <SkeletonContainer shown={loading} style={skeletonStyle}>
-          <CenterLine>
-            <Content>
-              {contractInfo.admin ? (
-                <AddressContainer
-                  value={contractInfo.admin}
-                  alias={
-                    isZeroAddress(contractInfo.admin)
-                      ? t(translations.general.zeroAddress)
-                      : undefined
-                  }
-                />
-              ) : (
-                notAvailableText
-              )}
-            </Content>
-            <WarnningButton key="warning" address={contractInfo.admin} />
-          </CenterLine>
-        </SkeletonContainer>
-      ),
-    },
-    // {
-    //   title: (
-    //     <Tooltip
-    //       text={t(translations.toolTip.contract.storageSponsor)}
-    //       placement="top"
-    //     >
-    //       {t(translations.contract.storageSponsor)}
-    //     </Tooltip>
-    //   ),
-    //   children: (
-    //     <SkeletonContainer shown={loading} style={skeletonStyle}>
-    //       <CenterLine>
-    //         <Content
-    //           className={clsx(
-    //             !contractInfo.sponsor.sponsorForCollateral && 'not-available',
-    //           )}
-    //         >
-    //           {contractInfo.sponsor &&
-    //           contractInfo.sponsor.sponsorForCollateral ? (
-    //             [
-    //               <AddressContainer
-    //                 key={contractInfo.sponsor.sponsorForCollateral}
-    //                 value={contractInfo.sponsor.sponsorForCollateral}
-    //                 alias={
-    //                   contractInfo.sponsor.sponsorForCollateralContractInfo &&
-    //                   contractInfo.sponsor.sponsorForCollateralContractInfo.name
-    //                     ? contractInfo.sponsor.sponsorForCollateralContractInfo
-    //                         .name
-    //                     : null
-    //                 }
-    //               />,
-    //             ]
-    //           ) : (
-    //             <CenterLine>{notAvailableText}</CenterLine>
-    //           )}
-    //         </Content>
-    //       </CenterLine>
-    //     </SkeletonContainer>
-    //   ),
-    // },
-    // {
-    //   title: (
-    //     <Tooltip
-    //       text={t(translations.toolTip.contract.gasFeeSponsor)}
-    //       placement="top"
-    //     >
-    //       {t(translations.contract.gasSponsor)}
-    //     </Tooltip>
-    //   ),
-    //   children: (
-    //     <SkeletonContainer shown={loading} style={skeletonStyle}>
-    //       <CenterLine>
-    //         <Content>
-    //           {contractInfo.sponsor && contractInfo.sponsor.sponsorForGas ? (
-    //             [
-    //               <AddressContainer
-    //                 key={contractInfo.sponsor.sponsorForGas}
-    //                 value={contractInfo.sponsor.sponsorForGas}
-    //                 alias={
-    //                   contractInfo.sponsor.sponsorForGasContractInfo &&
-    //                   contractInfo.sponsor.sponsorForGasContractInfo.name
-    //                     ? contractInfo.sponsor.sponsorForGasContractInfo.name
-    //                     : null
-    //                 }
-    //               />,
-    //             ]
-    //           ) : (
-    //             <CenterLine>{notAvailableText}</CenterLine>
-    //           )}
-    //         </Content>
-    //       </CenterLine>
-    //     </SkeletonContainer>
-    //   ),
-    // },
   ];
 
   if (![NETWORK_TYPES.mainnet, NETWORK_TYPES.testnet].includes(NETWORK_TYPE)) {
