6e88b1b8207a477c82ca35ca78bcaf7eb22c29c0
tangxuezhi <0x74616e67@gmail.com>
2022-05-24 09:43:14 +0800

    feat: update release note and link

--- src/app/containers/Notices/index.tsx
+++ src/app/containers/Notices/index.tsx
@@ -12,8 +12,8 @@ export const noticeInfo = {
   hot: false,
   type: 'Announcement',
   brief: {
-    en: 'ConfluxScan V1.4.2 Released!',
-    zh: 'ConfluxScan V1.4.2 发布喽！',
+    en: 'ConfluxScan V1.5.0 Released!',
+    zh: 'ConfluxScan V1.5.0 发布喽！',
   },
   link: {
     en:
