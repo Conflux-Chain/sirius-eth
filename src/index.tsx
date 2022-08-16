ddd3290d347142598b94ae788915d3b7c88a2894
0x74616e67 <0x74616e67@gmail.com>
2022-08-16 15:16:02 +0800

    feat: update release note and version number

--- src/index.tsx
+++ src/index.tsx
@@ -63,7 +63,7 @@ Promise.all([completeDetect(), completeDetectEthereum()]).then(() => {
   render(App);
 });
 
-const currentVersion = 'v1.8.0';
+const currentVersion = 'v1.9.0';
 
 const brand = `
 ╔═╗┌─┐┌┐┌┌─┐┬  ┬ ┬─┐ ┬  ┌─┐╔═╗┌─┐┌─┐┌─┐┌─┐ ${currentVersion}
