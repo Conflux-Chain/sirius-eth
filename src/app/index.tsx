b59fbe514af0faa7d3c8a7469b3bae424d1f3783
0x74616e67 <0x74616e67@gmail.com>
2023-08-04 10:32:37 +0800

    feat: update hidden info for .net env

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -63,8 +63,8 @@ import { Contract } from './containers/Contract/Loadable';
 import { TokenDetail } from './containers/TokenDetail/Loadable';
 
 import Loading from 'app/components/Loading';
-import { CookieTip } from './components/CookieTip';
-import { GlobalTip } from './components/GlobalTip';
+// import { CookieTip } from './components/CookieTip';
+// import { GlobalTip } from './components/GlobalTip';
 
 // import { Swap } from './containers/Swap';
 import { ContractDeployment } from './containers/ContractDeployment/Loadable';
@@ -655,8 +655,8 @@ export function App() {
                     </Main>
                     <Footer />
                     <GlobalStyle />
-                    <CookieTip />
-                    <GlobalTip tipKey="addressWarning" />
+                    {/* <CookieTip />
+                    <GlobalTip tipKey="addressWarning" /> */}
                   </>
                 )}
                 <GlobalNotify />
