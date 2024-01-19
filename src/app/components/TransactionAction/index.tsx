b327cd2948f9b514f6e5ec36c11cc56fc5b3b5d5
shine <shouhui.sun@gmail.com>
2024-01-19 11:01:29 +0800

    fix: some bugs

--- src/app/components/TransactionAction/index.tsx
+++ src/app/components/TransactionAction/index.tsx
@@ -153,7 +153,7 @@ const customUI: MultiAction = {
     return (
       <div style={{ ...StyleWrap }}>
         Transfer <BalanceStyle>{value}</BalanceStyle> of{' '}
-        {Token(address, customInfo, 'ERC721', ['symbol'])}
+        {Token(address, customInfo, 'ERC721', ['icon', 'name', 'symbol'])}
       </div>
     );
   },
