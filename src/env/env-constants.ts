6a9745959ce1f65ddc694d71c67fab4f645fe664
mosi <mosshengqi@outlook.com>
2024-02-28 15:48:19 +0800

    fix: env check

--- src/env/env-constants.ts
+++ src/env/env-constants.ts
@@ -6,19 +6,19 @@ export const IS_ESPACE_DEVNET =
   /^net[\d]+eth/.test(window.location.host);
 export const IS_ESPACE_TESTNET =
   process.env.REACT_APP_EVM_TESTNET === 'true' ||
-  /evmtestnet\./.test(window.location.hostname);
+  /^evmtestnet[.-]/.test(window.location.hostname);
 export const IS_ESPACE_MAINNET =
   process.env.REACT_APP_EVM_MAINNET === 'true' ||
-  /evm\./.test(window.location.hostname);
+  /^evm[.-]/.test(window.location.hostname);
 export const IS_BSPACE_DEVNET =
   process.env.REACT_APP_BTC_DEVNET === 'true' ||
   /^net[\d]+btc/.test(window.location.host);
 export const IS_BSPACE_TESTNET =
   process.env.REACT_APP_BTC_TESTNET === 'true' ||
-  /btctestnet\./.test(window.location.hostname);
+  /^btctestnet[.-]/.test(window.location.hostname);
 export const IS_BSPACE_MAINNET =
   process.env.REACT_APP_BTC_MAINNET === 'true' ||
-  /btc\./.test(window.location.hostname);
+  /^btc[.-]/.test(window.location.hostname);
 
 export const IS_ESPACE =
   IS_ESPACE_MAINNET || IS_ESPACE_TESTNET || IS_ESPACE_DEVNET;
