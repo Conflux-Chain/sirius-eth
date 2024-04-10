05b2fc416df13c1fe1519d2d69e53a1b67bbbdc4
cryptosunshine <shouhui.sun@gmail.com>
2024-04-10 19:40:52 +0800

    fix: add API_HOST_MAP

--- src/env/env-constants.ts
+++ src/env/env-constants.ts
@@ -1,5 +1,6 @@
-export const DOMAIN = window.location.hostname.includes('.io') ? '.io' : '.net';
+import { LOCALSTORAGE_KEYS_MAP } from 'utils/enum';
 export const IS_FOREIGN_HOST = /.io$/.test(window.location.host);
+export const DOMAIN = IS_FOREIGN_HOST ? '.io' : '.net';
 
 export const IS_ESPACE_DEVNET =
   process.env.REACT_APP_EVM_DEVNET === 'true' ||
@@ -32,3 +33,17 @@ export const IS_STAGE = process.env.REACT_APP_DEV === 'true';
 export const IS_DEV = process.env.NODE_ENV === 'development';
 
 export const STAGE_FLAG = IS_STAGE ? '-stage' : '';
+
+export const API_HOST_MAP: {
+  rpcHost?: string;
+  openAPIHost?: string;
+  secondaryOpenAPIHost?: string;
+  secondaryBackendAPIHost?: string;
+} = (() => {
+  try {
+    const apis = localStorage.getItem(LOCALSTORAGE_KEYS_MAP.apis) ?? '';
+    return JSON.parse(apis);
+  } catch (error) {
+    return {};
+  }
+})();
