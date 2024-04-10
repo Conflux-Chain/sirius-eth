f5e02d6a60c50d10f94de7cdf247d6c248a4edd2
cryptosunshine <shouhui.sun@gmail.com>
2024-04-10 19:40:52 +0800

    fix: add API_HOST_MAP

--- src/env/espace/testnet.ts
+++ src/env/espace/testnet.ts
@@ -1,21 +1,29 @@
-import { DOMAIN, IS_STAGE } from 'env/env-constants';
+import { API_HOST_MAP, DOMAIN, IS_STAGE } from 'env/env-constants';
 import { NETWORK_TYPES } from 'env/types';
 import logo from 'images/espace/logo-testnet.svg';
 export * from './base';
 export { default as ENV_LOCALES_EN } from 'sirius-next/packages/i18n/evm/espace/en/translation.json';
 export { default as ENV_LOCALES_CN } from 'sirius-next/packages/i18n/evm/espace/zh_cn/translation.json';
+
 export const ENV_NETWORK_ID = 71;
 export const ENV_NETWORK_TYPE = NETWORK_TYPES.EVM_TESTNET;
-export const ENV_API_HOST = IS_STAGE
-  ? `evmapi-testnet-stage.confluxscan${DOMAIN}`
-  : `evmapi-testnet.confluxscan${DOMAIN}`;
-export const ENV_CORE_API_HOST = IS_STAGE
-  ? `api-testnet-stage.confluxscan${DOMAIN}`
-  : `api-testnet.confluxscan${DOMAIN}`;
-export const ENV_CORE_SCAN_HOST = IS_STAGE
-  ? `testnet-stage.confluxscan${DOMAIN}`
-  : `testnet.confluxscan${DOMAIN}`;
-export const ENV_RPC_SERVER = 'https://evmtestnet-cfxbridge.confluxrpc.com';
+export const ENV_API_HOST =
+  API_HOST_MAP.openAPIHost ||
+  (IS_STAGE
+    ? `https://evmapi-testnet-stage.confluxscan${DOMAIN}`
+    : `https://evmapi-testnet.confluxscan${DOMAIN}`);
+export const ENV_CORE_API_HOST =
+  API_HOST_MAP.secondaryOpenAPIHost ||
+  (IS_STAGE
+    ? `https://api-testnet-stage.confluxscan${DOMAIN}`
+    : `https://api-testnet.confluxscan${DOMAIN}`);
+export const ENV_CORE_SCAN_HOST =
+  API_HOST_MAP.secondaryBackendAPIHost ||
+  (IS_STAGE
+    ? `https://testnet-stage.confluxscan${DOMAIN}`
+    : `https://testnet.confluxscan${DOMAIN}`);
+export const ENV_RPC_SERVER =
+  API_HOST_MAP.rpcHost || 'https://evmtestnet-cfxbridge.confluxrpc.com';
 export const ENV_WALLET_CONFIG = {
   chainId: ENV_NETWORK_ID,
   chainName: 'eSpace Testnet',
