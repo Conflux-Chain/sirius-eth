b8f6564d0b81448a81b26a829cd5c48647c0439a
mosi <mosshengqi@outlook.com>
2024-04-18 11:36:53 +0800

    feat: delete PackingPage

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -390,15 +390,6 @@ export const GlobalStyle = createGlobalStyle`
     }
   }
 
-  .sirius-next-tooltip {
-    a {
-      color: var(--theme-color-blue0);
-
-      &:hover {
-        color: var(--theme-color-blue2);
-      }
-    }
-  }
   .ant-tooltip {
     a {
       color: var(--theme-color-blue0);
