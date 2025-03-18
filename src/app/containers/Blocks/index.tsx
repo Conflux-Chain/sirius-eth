55a825fde15e2a851a0bae2e27a9fd2bfceaee58
mosi <mosshengqi@outlook.com>
2025-03-18 15:19:24 +0800

    optimize: column width

--- src/app/containers/Blocks/index.tsx
+++ src/app/containers/Blocks/index.tsx
@@ -12,7 +12,7 @@ export function Blocks() {
   const url = '/block';
   const [ageFormat, toggleAgeFormat] = useAge();
 
-  const columnsBlocksWidth = [4, 4, 3, 4, 4, 5, 4, 4, 5];
+  const columnsBlocksWidth = [4, 4, 3, 5, 4, 5, 4, 4, 5];
   const columnsBlocks = [
     blockColunms.epoch,
     blockColunms.hashWithPivot,
