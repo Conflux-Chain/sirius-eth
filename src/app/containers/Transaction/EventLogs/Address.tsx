21a9551e2696bfc5ecdc3622859b068a4fb754a3
mosi <mosshengqi@outlook.com>
2026-06-01 11:37:54 +0800

    feat: update  sirius-next version

--- src/app/containers/Transaction/EventLogs/Address.tsx
+++ src/app/containers/Transaction/EventLogs/Address.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from '@cfxjs/sirius-next-common/dist/components/Link';
+import { ContractDetail } from '@cfxjs/sirius-next-common/dist/components/InputData/ContractDetail';
 import { formatAddress } from 'utils';
-import { ContractDetail } from 'app/components/TxnComponents/ContractDetail';
 import { convertCheckSum } from '@cfxjs/sirius-next-common/dist/utils/address';
 import { StyledHighlight } from './StyledComponents';
 
@@ -12,7 +12,7 @@ export const Address = ({ address }) => {
       <StyledHighlight scope="address" value={hex}>
         <Link href={`/address/${hex}`}>{hex}</Link>
       </StyledHighlight>
-      <ContractDetail address={address} />
+      <ContractDetail address={address} addressType="hex" />
     </>
   );
 };
